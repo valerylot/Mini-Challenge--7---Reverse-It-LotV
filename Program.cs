cdd3ce7163a3c9ec9ec70fe5586f4eead9e47d1d
valerylot <lotvalery@gmail.com>
2026-09-11 15:57:21 -0700

    First commit Mini Challenge #7

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Valery Lot
+//09/11/2026
+//Mini Challenge #7 Reverse It
+//We used a for loop to reverse a number sequence by decrementing it. Then we used a while loop if the user wanted to play the game again.
+//Peer Review Name:
+//Review:
+
+Console.Clear();
+
+string playAgain;
+
+do
+{
+Console.WriteLine("Please enter a number sequence.");
+string number = Console.ReadLine();
+
+Console.WriteLine($"Your number sequence is: {number}");
+
+//for loop
+string numberReverse = "";
+
+for(int i = number.Length; i > 0; i--)
+{
+    numberReverse += number[i-1];
+}
+Console.WriteLine($"Your number sequence in reverse is: {numberReverse}");
+
+
+//play again loop
+Console.WriteLine("Do you want to play again? Y / N ");
+playAgain = Console.ReadLine().ToUpper();
+
+while (playAgain != "Y" && playAgain != "N")
+    {
+        Console.WriteLine("Invalid input. Please enter Y / N ");
+        playAgain = Console.ReadLine().ToUpper();
+    }
+
+} while(playAgain == "Y");
